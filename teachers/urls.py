524971a5467d1bb12bcac947f689ea42ac1178a8
Hasanboy Nasirdinov <164983145+khasanboy-06@users.noreply.github.com>
2024-06-11 19:08:26 +0500

    erp

--- teachers/urls.py
+++ teachers/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TeacherDashboardView
+from .views import TeacherDashboardView, TeacherTeamsView, LessonView
 
 
 app_name = 'teachers'
 
 urlpatterns = [
     path('teacher/', TeacherDashboardView.as_view(), name='teacher'),
+    path('guruhlarim/', TeacherTeamsView.as_view(), name='guruhlarim'),
+    path('lessons/<int:group_id>/', LessonView.as_view(), name='lessons'),
 ]
--- teachers/views.py
+++ teachers/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from users.permissions import TeacherRequiredMixin
+from users.models import Teacher, Team
 
 
 class TeacherDashboardView(TeacherRequiredMixin, View):
     def get(self, request):
         return render(request, 'teachers/teacher_dashboard.html')
+    
+
+
+class TeacherTeamsView(TeacherDashboardView, View):
+    def get(self, request):
+        teacher = get_object_or_404(Teacher, user = request.user)
+        teams = teacher.teams.all()
+        return render(request, 'teachers/guruhlarim.html', context={"teams":teams})
+    
+
+class LessonView(View):
+    def get(self, request, group_id):
+        team = get_object_or_404(Team, id=group_id)
+        lessons = team.lessons.all()
+        return render(request, 'teachers/lessons.html', context={"lessons":lessons})
