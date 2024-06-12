31911f134b794d539a69664ba160dec32a523076
Hasanboy Nasirdinov <164983145+khasanboy-06@users.noreply.github.com>
2024-06-12 12:33:48 +0500

    erp

--- teachers/urls.py
+++ teachers/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import TeacherDashboardView, TeacherTeamsView, LessonView
-
+from .views import TeacherDashboardView, TeacherTeamsView, LessonView, TeamStudentsView
 
 app_name = 'teachers'
 
@@ -8,4 +7,5 @@ urlpatterns = [
     path('teacher/', TeacherDashboardView.as_view(), name='teacher'),
     path('guruhlarim/', TeacherTeamsView.as_view(), name='guruhlarim'),
     path('lessons/<int:group_id>/', LessonView.as_view(), name='lessons'),
+    path('students/', TeamStudentsView.as_view(), name='students'),
 ]
--- teachers/views.py
+++ teachers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from users.permissions import TeacherRequiredMixin
 from users.models import Teacher, Team
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class TeacherDashboardView(TeacherRequiredMixin, View):
@@ -22,3 +23,11 @@ class LessonView(View):
         team = get_object_or_404(Team, id=group_id)
         lessons = team.lessons.all()
         return render(request, 'teachers/lessons.html', context={"lessons":lessons})
+    
+
+class TeamStudentsView(TeacherRequiredMixin, View):
+    def get(self, request):
+        teacher = get_object_or_404(Teacher, user=request.user)
+        students = teacher.students.all()
+        return render(request, 'teachers/students.html', context={"students":students})
+    
